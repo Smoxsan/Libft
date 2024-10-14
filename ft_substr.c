fbc363d7f8e68813e8681131dfecb4f025ead78e
Smoxsan <smox@DESKTOP-C9JQ6RG>
2024-10-14 12:50:51 +0200

    substr

--- ft_substr.c
+++ ft_substr.c
@@ -1,4 +1,20 @@
 char *ft_substr(char const *s, unsigned int start, size_t len)
 {
-    
+    int max_len;
+
+    if (start < 0 || start >= strlen(s)) {
+        return (NULL);
+    }
+    max_len = strlen(s) - start;
+    if (len > max_len) {
+        len = max_len;
+    }
+    char* result = (char*) malloc((len + 1) * sizeof(char));
+    if (result == NULL) {
+        return NULL;
+    }
+    strncpy(result, s + start, len);
+    result[len] = '\0';
+
+    return result;
 }
