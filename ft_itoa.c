2b131c9254a158fbb36a6fa8e78eea4d2b6d6da0
Finn Konig <fkonig@1-A-11.42heilbronn.de>
2024-10-18 15:05:51 +0200

    ?

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fkonig <fkonig@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/18 15:02:45 by fkonig            #+#    #+#             */
+/*   Updated: 2024/10/18 15:02:47 by fkonig           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 char *ft_itoa(int n)
 {
 	int num;
@@ -11,10 +23,22 @@ char *ft_itoa(int n)
 		num = num/10;
 		count++;
 	}
-	result = (char*)malloc((count + 1) * sizeof(char*));
+	    if (n < 0)
+    {
+        result[0] = '-';
+        n = -n;
+    }
+    else if (n == 0)
+    {
+        result[0] = '0';
+        result[1] = '\0';
+        return result;
+    }
+	result = (char*)malloc((count + 1) * sizeof(char));
 	result[count] = '\0';
 	while(n >= 10)
 	{
 		result[count--] = n/10;
 	}
+	return result;
 }
