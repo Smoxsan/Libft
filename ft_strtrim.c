c46ef9a4aedacef060f5fb2a41cfbb6074e32aee
Finn Konig <fkonig@1-A-11.42heilbronn.de>
2024-10-18 15:25:51 +0200

    strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: fkonig <fkonig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 15:02:38 by fkonig            #+#    #+#             */
-/*   Updated: 2024/10/18 15:02:40 by fkonig           ###   ########.fr       */
+/*   Updated: 2024/10/18 15:15:46 by fkonig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,23 @@ char *ft_strtrim(char const *s1, char const *set)
     const char* start;
     const char* end;
     size_t new_len;
+    char *trimmed;
 
     if (s1 == NULL) {
         return NULL;
     }
     start = s1;
+    while (*start && strchr(set, *start))
+        start++;
     end = s1 + ft_strlen(s1) - 1;
-    while ()
+    while (end > start && ft_strchr(set, *(end - 1)))
         end--;
-    new_len = end - start + 1;
+    new_len = end - start;
+    trimmed = (char*)malloc(new_len + 1);
+    if(!trimmed)
+        return NULL;
+    ft_strlcpy(trimmed, start, new_len);
+    trimmed[new_len] = '\0';
 
+    return trimmed;
 }
