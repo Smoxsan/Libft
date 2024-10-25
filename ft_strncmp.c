efcf98b853f786c5e46bf730c2b4816ce4639e05
Smoxsan <smox@DESKTOP-C9JQ6RG>
2024-10-25 16:17:14 +0200

    strncmp fix

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fkonig <fkonig@student.42.fr>              +#+  +:+       +#+        */
+/*   By: smox <smox@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 11:53:29 by fkonig            #+#    #+#             */
-/*   Updated: 2024/10/23 16:52:40 by fkonig           ###   ########.fr       */
+/*   Updated: 2024/10/25 16:16:23 by smox             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,17 +23,17 @@ int	ft_strncmp(const char *str, const char *comp, size_t n)
 		n--;
 	}
 	if(n == 0)
-	{
 		return (0);
-	}
-	else
-		return ((unsigned char)str - (unsigned char)comp);
+	return ((unsigned char)*str - (unsigned char)*comp);
 }
-
-// int main()
-// {	
-// 	printf("%i\n", res);
-// 	int resx = strcmp(str, comp);
-// 	printf("%i\n", resx);
-// 	ft_strcmp("a=cde", "agcde");
-// }
+#include <stdio.h>
+int main(void)
+{
+    printf("Test Case 1: %d\n", ft_strncmp("abc", "abc", 3));   // Should return 0
+    printf("Test Case 2: %d\n", ft_strncmp("abc", "abcd", 3));  // Should return 0
+    printf("Test Case 3: %d\n", ft_strncmp("abc", "abz", 3));   // Should return a negative value
+    printf("Test Case 4: %d\n", ft_strncmp("abc", "ab", 3));    // Should return a positive value
+    printf("Test Case 5: %d\n", ft_strncmp("abc", "abcd", 5));  // Should return a negative value (out of bounds)
+    
+    return 0;
+}
