784863edcf54c3ec9dcd8882354566a6fc2aa38e
Smoxsan <smox@DESKTOP-C9JQ6RG>
2024-10-25 16:04:49 +0200

    strtrim fix

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: smox <smox@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 13:36:29 by fkonig            #+#    #+#             */
-/*   Updated: 2024/10/25 14:21:56 by smox             ###   ########.fr       */
+/*   Updated: 2024/10/25 15:58:17 by smox             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,32 +29,32 @@ char	*ft_strchr(const char *str, int search_str)
 
 	return (NULL);
 }
-#include <stdio.h>
-int main(void)
-{
-    const char *string = "Hello, World!";
-    int search_char = 'o'; // Character to search for
-    char *result;
+//#include <stdio.h>
+//int main(void)
+//{
+//    const char *string = "Hello, World!";
+//    int search_char = 'o'; // Character to search for
+//    char *result;
 
     // Call ft_strchr to find the character
-    result = ft_strchr(string, search_char);
+//    result = ft_strchr(string, search_char);
 
-    if (result != NULL)
-    {
-        printf("Found '%c' at position: %ld\n", search_char, result - string);
-    }
-    else
-    {
-        printf("Character '%c' not found.\n", search_char);
-    }
+//    if (result != NULL)
+//    {
+//        printf("Found '%c' at position: %ld\n", search_char, result - string);
+//    }
+//    else
+//    {
+//        printf("Character '%c' not found.\n", search_char);
+//    }
 
     // Test searching for the null terminator
-    search_char = '\0'; // Searching for the null terminator
-    result = ft_strchr(string, search_char);
-    if (result != NULL)
-    {
-        printf("Found null terminator at position: %ld\n", result - string);
-    }
+//    search_char = '\0'; // Searching for the null terminator
+//    result = ft_strchr(string, search_char);
+//    if (result != NULL)
+//    {
+//        printf("Found null terminator at position: %ld\n", result - string);
+//    }
 
-    return 0;
-}
+//    return 0;
+//}
